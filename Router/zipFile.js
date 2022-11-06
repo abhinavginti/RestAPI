3decfa317703afe54db22cb5911734d649a42d59
abhinavginti <abhinavginti@gmail.com>
2022-11-06 23:25:27 +0530

    unzip using cmd line

--- Router/zipFile.js
+++ Router/zipFile.js
@@ -13,7 +13,14 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({
-    storage: storage
+    storage: storage,
+    fileFilter: (req,file,cb) => {
+        if(file.mimetype === 'application/zip'){
+            cb(null,true);
+        }else{
+            cb(new Error("Not a zip File"),false)
+        }
+    }
 })
 
 router.post('/zipFile', upload.single('file'), zipFileController.decompressZipFile)
--- controller/zipFileController.js
+++ controller/zipFileController.js
@@ -1,18 +1,38 @@
-const decompress = require('decompress')
+// const decompress = require('decompress')
 const fs = require('fs')
+const { exec } = require('child_process')
 
 exports.decompressZipFile = async (req, res) => {
     try {
         if (req.file) {
-            await decompress('./Upload/' + req.file.filename, 'Upload')
-            fs.unlink('./Upload/' + req.file.filename, (err) => {
-                if (err) throw err;
-                console.log(req.file.filename + ' was deleted');
+            // await decompress('./Upload/' + req.file.filename, 'Upload',{
+            //     map: file => {
+            //         file.path = Date.now() + file.path
+            //         return file
+            //     }
+            // })
+            const destinationFolder = req.file.filename.substring(0, req.file.filename.lastIndexOf('.'))
+            const cmd = `cd "./Upload" && mkdir ${destinationFolder} && tar -xf "${req.file.filename}" -C ${destinationFolder}`
+            exec(cmd, (error, stdout, stderr) => {
+                fs.unlink('./Upload/' + req.file.filename, (err) => {
+                    if (err) throw err;
+                    console.log(req.file.filename + ' was deleted');
+                })
+                if (error) {
+                    console.error(error)
+                    return res.status(500).send({ error: error })
+
+                }
+                if (stderr) {
+                    console.error(stderr);
+                    return res.status(500).send({ error: stderr })
+                }
+                console.log(stdout)
+                res.send('File Successfully Uploaded')
             })
-            res.send('File Successfully Uploaded')
-        }else{
+        } else {
             throw "No file found ..."
-        }   
+        }
     } catch (err) {
         console.error(err)
         res.status(500).send({ error: err })
